824db509941d714f11a41f66f2e246ccaf95c67b
liu huizhong <326424553@qq.com>
2021-02-28 10:18:20 +0800

    packagist包 创建第一个方法

---
+++ src/Index.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace vic091;
+/**
+ * User: vic
+ * Date: 2021/2/28
+ * Time: 10:15 上午
+ */
+class Index
+{
+    public function getName($name)
+    {
+        return "获取值：".$name;
+    }
+}
